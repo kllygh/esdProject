1900ec4c61513276fcc8f13c45bf3deb123c8d81
kelly goh <kelly.goh.2021@scis.smu.edu.sg>
2023-03-19 01:55:04 +0800

    location.py
    Changes made during the testing phase version 2!

--- location.py
+++ location.py
@@ -14,10 +14,9 @@ CORS(app)
 
 #################### Converting Customer Location to Lat and Long ####################################
 def get_cust_location(address):
-    API_KEY = 'example'
+    #print("Start of get_cust_location")
 
-    #this is now hardcoded but need to change the address to collect from the NearBy MS
-    address = '1 hack drive, menlo park, CA'
+    API_KEY = 'AIzaSyDla7hHpUucckrVqSjdXTY_kUGHj8x2uUM'
 
     params  = {
         'key': API_KEY,
@@ -36,34 +35,45 @@ def get_cust_location(address):
             geometry = data['results'][0]['geometry']
             lat = geometry['location']['lat']
             lon = geometry['location']['lng']
-            print(f'Latitude: {lat}, Longitude: {lon}')
+            return ({'lat':lat, 'lng': lon})
         else:
             print(f'Geocoding failed. Status: {data["status"]}')
     except requests.exceptions.HTTPError as err:
         print(f'Error: {err}')
     except requests.exceptions.RequestException as err:
         print(f'Request error: {err}')
-
+    
+    #print("End of get_cust_location")
 
 ################# Finding the nearest location ########################################################
 
-
 @app.route('/location', methods = ['POST']) 
 def find_nearest_location():
-    data = request.json # {starting_loc:"sembawang close blk 234a" , rest_loc:[{lat:...., lng:.....,rest_id:.....},{lat:...., lng:.....},{lat:...., lng:.....}] }
 
-    data["starting_location"] = get_cust_location(data['starting_loc'])
+    print("Start of find_nearest_location")
+    data = request.json() # {starting_loc:"1 hack drive, menlo park, CA" , rest_loc:[{lat:...., lng:.....,rest_id:.....},{lat:...., lng:.....},{lat:...., lng:.....}] }
 
+    # return ({'lat':lat, 'lng': lon})
+    data["starting_location"] = get_cust_location(data.get('starting_location'))
+
+    #data = {starting_location: , rest_loc: }
     #starting_location = {lat:...., lng:.....}
-    #restaurant_location = [{lat:...., lng:.....},{lat:...., lng:.....},{lat:...., lng:.....}]
-    customer_location = data['starting_location'] #make sure that this is in the complex MS and under payload 
-    restaurant_location = data['restaurant_location'] #make sure that this is in the complex MS and under payload 
+    #restaurant_location = [{'lat':37.48007705012013, 'lng':-122.14532593683853},{'lat':37.48053919210831, 'lng':-122.15144075520307},{'lat':37.477040045901575, 'lng':-122.15352062600218},{'lat':37.477139080546856, 'lng':-122.16080017167964},{'lat':37.46779623020498, 'lng':-122.14041744300732},
+    #                       {'lat':37.46779623020498, 'lng':-122.1516903396774},{'lat':37.46600271717554, 'lng':-122.1568760578018},{'lat':37.46600271717554, 'lng':-122.1568760578018},{'lat':37.469206976332174, 'lng':-122.16603436035237},{'lat':37.469206976332174, 'lng':-122.16603436035237},
+    #                       {'lat':1.3823169660466292, 'lng':103.68905007456796},{'lat':37.465231300843634, 'lng':-122.18633215021532},{'lat':37.46888062276618, 'lng':-122.18027645639775},{'lat':37.48068777703546, 'lng':-122.16188508879591},{'lat':37.47789932093983, 'lng':-122.13844731073895}
+    #                       ]
+
+    # customer_location = data['starting_location'] #make sure that this is in the complex MS and under payload 
+    # restaurant_location = data['restaurant_location'] #make sure that this is in the complex MS and under payload 
+
+    customer_location = data.get('starting_location') #make sure that this is in the complex MS and under payload 
+    restaurant_location = data.get('restaurant_location') #make sure that this is in the complex MS and under payload 
 
     # process the locations
     top_20_locations = get_nearest_locations(customer_location['lat'],customer_location['lng'],restaurant_location)
 
     # check if the .json thing works for here
-    nearest_locations = top_20_locations.json()
+    nearest_locations = top_20_locations
     return jsonify({
         'status': 'success',
         'nearest_locations': nearest_locations
@@ -71,6 +81,7 @@ def find_nearest_location():
 
 
 def haversine(lat1, lon1, lat2, lon2):
+    print("Start of haversine")
     """
     Calculate the great circle distance between two points 
     on the Earth (specified in decimal degrees)
@@ -84,22 +95,22 @@ def haversine(lat1, lon1, lat2, lon2):
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
     r = 6371 # Radius of earth in kilometers
+    print("End of haversine")
     return c * r
 
 def get_nearest_locations(latitude, longitude, locations):
+    print("Start of get_nearest_locations")
     """
     Get the nearest top 20 locations based on the provided latitude, longitude, and list of locations
     """
     # Calculate distances between the given location and all locations in the list
     distances = []
     for location in locations:
-        dist = haversine(latitude, longitude, location['latitude'], location['longitude'])
+        dist = haversine(latitude, longitude, location['lat'], location['lng'])
         distances.append((location, dist))
     
     # Sort the list of locations by distance in ascending order and return the top 20 locations
     sorted_distances = sorted(distances, key=lambda x: x[1])
-    nearest_locations = [x[0] for x in sorted_distances[:20]]
-    
+    nearest_locations = [x[0] for x in sorted_distances[:14]]
+    print("End of get_nearest_locations")
     return nearest_locations
-
-
