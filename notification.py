c7179fab4dbfb8438fdee6f9362b05d337314947
JXChin02 <jxchin.2021@scis.smu.edu.sg>
2023-03-19 15:39:58 +0800

    Update notification.py

--- notification.py
+++ notification.py
@@ -13,7 +13,7 @@ monitorBindingKey='#.notif'
 def sendNotif():
     amqp_setup.check_setup()
         
-    queue_name = 'Send_notification' #can change later
+    queue_name = 'Notification' #can change later
     
     # set up a consumer and start to wait for coming messages
     amqp_setup.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
