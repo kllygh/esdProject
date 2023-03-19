219ec452d6846d2d5e50de96e40f1ff2c5928892
Jolene <jolenechewzhiqi@gmail.com>
2023-03-19 22:57:02 +0800

    Created order microservice

---
+++ order/order.py
@@ -0,0 +1,193 @@
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from sqlalchemy import exc
+
+from datetime import datetime
+import json
+from os import environ
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
+    'dbURL') or 'mysql+mysqlconnector://root@localhost:3306/order'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    order_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    customer_id = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, nullable=False)
+    boxID = db.Column(db.Integer, nullable=False)  # change to boxID
+    charge_id = db.Column(db.String(50), nullable=False, default='NaN')
+    refund_id = db.Column(db.String(50), nullable=False, default='NaN')
+    quantity = db.Column(db.Integer, nullable=False)
+    total_bill = db.Column(db.Numeric(precision=5, scale=2))
+    transaction_no = db.Column(db.Integer)
+    payment_method = db.Column(db.String(50))
+    currency = db.Column(db.String(50))
+    status = db.Column(db.String(10), nullable=False, default="NEW")
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    modified = db.Column(db.DateTime, nullable=False,
+                         default=datetime.now, onupdate=datetime.now)
+
+    # NOTE The __init__ method is not required because SQLAlchemy provides a default constructor for the class that handles the initialization of the attributes based on the database schema
+
+    def json(self):
+        order_info = {
+            'order_id': self.order_id,
+            'customer_id': self.customer_id,
+            'restaurant_id': self.restaurant_id,
+            'boxID': self.boxID,
+            'charge_id': self.charge_id,
+            'refund_id': self.refund_id,
+            'quantity': self.quantity,
+            'total_bill': self.total_bill,
+            'transaction_no': self.transaction_no,
+            'payment_method': self.payment_method,
+            'currency': self.currency,
+            'status': self.status,
+            'created': self.created,
+            'modified': self.modified
+        }
+
+        return order_info
+
+
+@app.route("/order/customer/<int:customer_id>")
+def get_all(customer_id):
+    # orderlist = Order.query.all()
+    orderlist = Order.query.filter_by(customer_id=customer_id).all()
+    # list of Order objects.
+    if len(orderlist):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "orders": [order.json() for order in orderlist]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no orders."
+        }
+    ), 404
+
+
+@app.route("/order/<int:order_id>")
+def find_by_order_id(order_id):
+    order = Order.query.filter_by(order_id=order_id).first()
+    if order:
+        return jsonify(
+            {
+                "code": 200,
+                "data": order.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "data": {
+                "order_id": order_id
+            },
+            "message": "Order not found."
+        }
+    ), 404
+
+
+@app.route("/order/new", methods=['POST'])
+def create_order():
+    data = request.get_json()
+    order = Order(status="NEW", **data)
+
+    try:
+        db.session.add(order)
+        db.session.commit()
+
+    except exc.IntegrityError as e:
+        # rollback the transaction to avoid partially committing the changes
+        db.session.rollback()
+        return jsonify(
+            {
+                "code": 400,
+                "message": "An order with this ID already exists."
+            }
+        ), 400
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred while creating the order. " + str(e)
+            }
+        ), 500
+
+    # convert a JSON object to a string and print
+    print(json.dumps(order.json(), default=str))
+    print()
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": order.json()
+        }
+    ), 201
+
+
+@app.route("/order/update/<int:order_id>", methods=['PUT'])
+def update_order(order_id):
+    order = Order.query.filter_by(order_id=order_id).first()
+    # update payment details also
+
+    # if no order with that id
+    if not order:
+        return jsonify(
+            {
+                "code": 404,
+                "data": {
+                    "order_id": order_id
+                },
+                "message": "Order not found."
+            }
+        ), 404
+
+    # update status
+    data = request.get_json()
+    for field in ['status', 'charge_id', 'refund_id', 'payment_method', 'currency', ]:
+        if field in data:
+            # The setattr() function sets the value of the specified attribute of the specified object.
+            setattr(order, field, data[field])
+    try:
+        db.session.commit()
+
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "order_id": order_id
+                },
+                "message": "An error occurred while updating the order. " + str(e)
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 200,
+            "message": "Order updated successfully.",
+            "data": order.json()
+        }
+    ), 200
+
+
+if __name__ == '__main__':
+    print("This is flask for " + os.path.basename(__file__) + ": manage orders ...")
+    app.run(host='0.0.0.0', port=5001, debug=True)
---
+++ order/order.sql
@@ -0,0 +1,64 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- Once a transaction has been started, any subsequent SQL statements executed as part of that transaction will not be visible to other database users until the transaction has been completed. This ensures that the database remains in a consistent state, even if multiple users are accessing and modifying the data simultaneously.
+
+-- In addition to START TRANSACTION, SQL also provides other transaction control statements such as COMMIT and ROLLBACK, which are used to either commit the changes made in a transaction or to roll back the changes made in the event of an error or exception.
+
+--
+-- Database: `order`
+--
+CREATE DATABASE IF NOT EXISTS `order` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `order`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `restaurant_id` int NOT NULL,
+  `boxID` int NOT NULL,
+  `charge_id` varchar(50) NOT NULL,
+  `refund_id` varchar(50) NOT NULL,
+  `quantity` int,
+  `total_bill` decimal(5,2),
+  `transaction_no` int,
+  `payment_method` varchar(50),
+  `currency` varchar(50),
+  `status` varchar(10) NOT NULL DEFAULT 'NEW',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `orders` (`order_id`, `customer_id`, `restaurant_id`, `boxID`, `charge_id`, `refund_id`, `quantity`, `total_bill`, `transaction_no`, `payment_method`, `currency`, `status`, `created`, `modified`) VALUES
+(1, 100, 02020202 , 123456789, '908', 'NULL', 3, 9.00, 908070605, 'Credit Card', 'SGD', 'NEW', '2020-06-12 02:14:55', '2020-06-12 02:14:55');
+
+INSERT INTO `orders` (`order_id`, `customer_id`, `restaurant_id`, `boxID`, `charge_id`, `refund_id`, `quantity`, `total_bill`, `transaction_no`, `payment_method`, `currency`, `status`, `created`, `modified`) VALUES
+(2, 100, 02020202 , 123456789, '908', 'NULL', 3, 9.00, 908070605, 'Credit Card', 'SGD', 'NEW', '2020-06-12 02:14:55', '2020-06-12 02:14:55');
+COMMIT;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+select * from orders
