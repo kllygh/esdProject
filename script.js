efbdff78cbc6440804acc16c3fe6bc1836e5c69c
KlariceT <klariceting.2021@scis.smu.edu.sg>
2023-03-29 18:59:04 +0800

    debugging

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const stripe = Stripe('pk_test_51M25gLJNS4gB3cUWC9UQDBWnQfcivrY2TdhUL0PGSTh5Gsu2
 const cardnum = document.querySelector('#cardnum')
 const cardexp = document.querySelector('#cardexp')
 const cardcvc = document.querySelector('#cardcvc')
-const btn = document.querySelector('#PlaceOrder')
+var btn = document.querySelector('#PlaceOrder');
 const sts = document.querySelector('.status')
 
 const mystyle={
@@ -25,30 +25,31 @@ expElm.mount(cardexp)
 const cvcElm = elements.create('cardCvc',{style:mystyle})
 cvcElm.mount(cardcvc)
 
-btn.addEventListener('click', ()=>{
-    fetch('paymentIntent.php', {
-        method:'POST', 
-        headers:{'Content-Type': 'application/json'},
-        body:{}
-    })
-    .then(res=>res.json())
-    .then(payload => {
-        stripe.confirmCardPayment(payload.client_secret, {
-            payment_method:{card:numElm}
-        }).then(transStat => {
-            if(transStat.error){
-                sts.innerHTML = `
-                <strong>Error:  </string> ${transStat.error.message}
-                `
-            }
-            else{
-                sts.innerHTML = `
-               <h3>${transStat.paymentIntent.description}</h3>
-               <strong>Transction Id: </strong>${transStat.paymentIntent.id}<br>
-               <strong>Amount deducted: </strong> ${transStat.paymentIntent.amount/100} ${transStat.paymentIntent.currency}
-                `
-            }
-            sts.style.display='block'
-        })
-    })
+btn.addEventListener("click", function(){
+    // fetch('./paymentIntent.php', {
+    //     method:'POST', 
+    //     headers:{'Content-Type': 'application/json'},
+    //     body:{}
+    // })
+    // .then(res=>res.json())
+    // .then(payload => {
+    //     stripe.confirmCardPayment(payload.client_secret, {
+    //         payment_method:{card:numElm}
+    //     }).then(transStat => {
+    //         if(transStat.error){
+    //             sts.innerHTML = `
+    //             <strong>Error:  </string> ${transStat.error.message}
+    //             `
+    //         }
+    //         else{
+    //             sts.innerHTML = `
+    //            <h3>${transStat.paymentIntent.description}</h3>
+    //            <strong>Transction Id: </strong>${transStat.paymentIntent.id}<br>
+    //            <strong>Amount deducted: </strong> ${transStat.paymentIntent.amount/100} ${transStat.paymentIntent.currency}
+    //             `
+    //         }
+    //         sts.style.display='block'
+    //     })
+    // })
+    alert("button clicked!")
 })
