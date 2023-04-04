375bc07b94b19d2c0865690bf8e97f9041ddb43b
jolenechew <jolenechewzhiqi@gmail.com>
2023-04-04 11:24:38 +0800

    eedited box and idex html

--- restaurant.py
+++ restaurant.py
@@ -4,15 +4,15 @@ from flask_cors import CORS
 from os import environ
 
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dburl')
-# 'mysql+mysqlconnector://root@localhost:3306/restaurant'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
+    'dburl') or 'mysql+mysqlconnector://root@localhost:3306/restaurant'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-CORS(app)  
+CORS(app)
+
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
@@ -23,7 +23,6 @@ class Restaurant(db.Model):
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
 
-
     def __init__(self, restaurant_id, restaurant_name, restaurant_location, latitude, longitude):
         self.restaurant_id = restaurant_id
         self.restaurant_name = restaurant_name
@@ -31,9 +30,8 @@ class Restaurant(db.Model):
         self.latitude = latitude
         self.longitude = longitude
 
-
     def json(self):
-        return {"restaurant_id": self.restaurant_id, "restaurant_name": self.restaurant_name, "restaurant_location": self.restaurant_location, "latitude": self.latitude, "longitude":self.longitude}
+        return {"restaurant_id": self.restaurant_id, "restaurant_name": self.restaurant_name, "restaurant_location": self.restaurant_location, "latitude": self.latitude, "longitude": self.longitude}
 
 
 #################### GET ALL RESTAURANTS #################################
@@ -60,7 +58,8 @@ def get_all():
 #################### GET A RESTAURANT BY ID #################################
 @app.route("/restaurant/<int:restaurant_id>")
 def find_by_restaurant_id(restaurant_id):
-    restaurant = Restaurant.query.filter_by(restaurant_id=restaurant_id).first()
+    restaurant = Restaurant.query.filter_by(
+        restaurant_id=restaurant_id).first()
     if restaurant:
         return jsonify(
             {
@@ -76,7 +75,6 @@ def find_by_restaurant_id(restaurant_id):
     ), 404
 
 
-
 #################### FILTER BY RESTAURANT_ID #################################
 @app.route("/restaurant", methods=["POST"])
 def get_by_restaurant_id():
@@ -86,10 +84,12 @@ def get_by_restaurant_id():
         restaurant_ids = data["restaurant_ids"]
 
         # Retrieve entries from the database that match the restaurant_ids
-        restaurant_entries = Restaurant.query.filter(Restaurant.restaurant_id.in_(restaurant_ids)).all()
+        restaurant_entries = Restaurant.query.filter(
+            Restaurant.restaurant_id.in_(restaurant_ids)).all()
 
         # Create a nested list containing the data for each entry
-        restaurant_entry_data = [[entry.restaurant_id, entry.restaurant_location, entry.restaurant_name, entry.latitude, entry.longitude] for entry in restaurant_entries]
+        restaurant_entry_data = [[entry.restaurant_id, entry.restaurant_location,
+                                  entry.restaurant_name, entry.latitude, entry.longitude] for entry in restaurant_entries]
 
         # Return a JSON response containing the list of arrays
         return jsonify({
@@ -106,13 +106,3 @@ def get_by_restaurant_id():
 
 if __name__ == '__main__':
     app.run(port=5300, host='0.0.0.0', debug=True)
-
-
-
-
-
-
-
-
-
-
